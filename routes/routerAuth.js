e7faba98a3b8fe5b863f9e571d34df6f535e1474
cesaraugusto316 <cesaraugusto316@protonmail.com>
2022-09-12 20:23:26 -0500

    comments deleted

--- routes/routerAuth.js
+++ routes/routerAuth.js
@@ -4,8 +4,5 @@ import { controllerAuth } from '../controllers/index.js';
 
 export const routerAuth = Router();
 
-// signUp
 routerAuth.route('/signup/').post(controllerAuth.signUp);
 routerAuth.route('/signin/').post(controllerAuth.signIn);
-
-// signIn
